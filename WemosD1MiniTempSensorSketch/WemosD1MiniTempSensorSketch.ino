036e54352dceeb3ab1057ad465efd91f780d9a37
Armindo Maurits <armindo.maurits@hotmail.com>
2018-04-01 14:08:10 +0200

    Initial commit

--- WemosD1MiniTempSensorSketch/WemosD1MiniTempSensorSketch.ino
+++ WemosD1MiniTempSensorSketch/WemosD1MiniTempSensorSketch.ino
@@ -1,31 +1,3 @@
-/*
- Name:		WemosD1MiniTempSensorSketch.ino
- Created:	4/1/2018 1:10:46 PM
- Author:	A. Maurits
-*/
-
-//#include <WEMOS_SHT3X.h>
-//
-//SHT3X sht30(0x45);
-
-//// the setup function runs once when you press reset or power the board
-//void setup() {
-//	Serial.begin(115200);
-//}
-//
-//// the loop function runs over and over again until power down or reset
-//void loop() {
-//	sht30.get();
-//	Serial.print("Temperature in Celsius : ");
-//	Serial.println(sht30.cTemp);
-//	Serial.print("Temperature in Fahrenheit : ");
-//	Serial.println(sht30.fTemp);
-//	Serial.print("Relative Humidity : ");
-//	Serial.println(sht30.humidity);
-//	Serial.println();
-//	delay(1000);
-//}
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -38,6 +10,7 @@ SHT3X sht30(0x45);
 
 void setup() {
 	Serial.begin(115200);
+
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
 	display.display();
 }
@@ -51,14 +24,17 @@ void loop() {
 
 	display.println("- SHT30 -");
 	display.print("T ");
-	display.print(sht30.cTemp);
+	display.print((int) sht30.cTemp);
 	display.println(" C");
 
 	display.print("H ");
-	display.print(sht30.humidity);
+	display.print((int) sht30.humidity);
 	display.println(" %");
 	display.println("");
 	display.println("AMA");
 	display.display();
+
+	Serial.print(sht30.cTemp);
+
 	delay(2000);
 }
